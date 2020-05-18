64f01ff9af8d55828ed22275d17c386f2a9aabd3
huageyiyangdewo <1981097256@qq.com>
2020-05-18 22:08:46 +0800

    使用栈完成逆波兰表达式的计算的算法

---
+++ stack/reserve_polish_notation.py
@@ -0,0 +1,27 @@
+from stack.stack_link import StackLink
+
+
+class ReservePolishNotation(object):
+
+    @staticmethod
+    def calculate(expression):
+        '''
+        计算逆波兰表达式的结果
+        :param expression:
+        :return:
+        '''
+        new_stack = StackLink()
+        for exp in expression:
+            if exp in ['+', '-', '*', '/']:
+                o1 = new_stack.pop()
+                o2 = new_stack.pop()
+                if not o1 or not o2:
+                    return False
+
+                t_str = o1 + exp + o2
+                ret = eval(t_str)
+                new_stack.push(str(ret))
+            else:
+                new_stack.push(exp)
+
+        return new_stack.pop()
---
+++ test/reserve_polish_notation_test.py
@@ -0,0 +1,5 @@
+from stack.reserve_polish_notation import ReservePolishNotation
+
+
+a = ['1', '2', '*', '3', '4', '*', '+', '5', '/']
+print(ReservePolishNotation.calculate(a))
