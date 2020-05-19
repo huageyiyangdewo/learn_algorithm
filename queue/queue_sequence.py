1e6bb7687c4a3b79a5d7fbc92cc56e1ff4dada5f
huageyiyangdewo <1981097256@qq.com>
2020-05-19 20:16:43 +0800

    实现队列通过列表

---
+++ queue/queue_sequence.py
@@ -0,0 +1,19 @@
+class Queue(object):
+    """队列"""
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        """进队列"""
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        """出队列"""
+        return self.items.pop()
+
+    def size(self):
+        """返回大小"""
+        return len(self.items)
